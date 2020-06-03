069c50c7396c9d3a6b0dd10336d1611d57f44955
kanp7 <tatsuya715tk@gmail.com>
2020-06-04 00:08:15 +0900

    Add フォロー・フォロワー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,27 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships, class_name: "Relationship",
+            foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  #ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  #ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
   attachment :profile_image
 
   validates :name, presence: true, length: { minimum: 2 , maximum: 20}
